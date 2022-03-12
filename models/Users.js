c0f7ace645b88ed6bf00d75b2b8742e3e9498b17
Debbie Hall <debleehall15@gmail.com>
2022-03-12 16:56:42 -0500

    user model finished

--- models/Users.js
+++ models/Users.js
@@ -2,19 +2,39 @@ const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema(
     {
-        username:{},
-        email: {},
-        thoughts: {},
-        friends: {}
+        username:{
+            type: String,
+            required: true,
+            trim: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Users'
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
-);
+)
 
 UserSchema.virtual('friendCount').get(function() {
-    return this.friends.reduce(
-        (total, friend) => total + friend.replies.legth +1,
-        0
-    );
-});
+    return this.friends.length
+})
 
 const User = model('User', UserSchema);
 
